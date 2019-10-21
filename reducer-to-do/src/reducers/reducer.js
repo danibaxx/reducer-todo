869696359c8521ca58582a25fe72420f0dda2c19
Danielle B <danibaxx@hotmail.com>
2019-10-20 18:37:39 -0600

    CRA, made reducer and components folder, made reducer.js and Todo.js, initial state and reducer set-up in reducer.js

---
+++ reducer-to-do/src/reducers/reducer.js
@@ -0,0 +1,22 @@
+export const initialState = [
+  {
+  item: 'Watch Lecture',
+  completed: false,
+  id: 1
+},
+{
+  item: 'Do Project',
+  completed: false,
+  id: 2
+},
+{
+  item: 'Remember What You Learned',
+  completed: false,
+  id: 3
+}
+]
+
+export function reducer(state, action) {
+  return {
+  }
+}
